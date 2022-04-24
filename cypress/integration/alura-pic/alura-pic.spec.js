c1f7afcc59109d0fc37473f8b310c8bd08220e6a
Rubendvb <rubenvasquez556@gmail.com>
2022-04-24 03:53:37 -0300

    Criando primeiros casos de testes

--- cypress/integration/alura-pic/alura-pic.spec.js
+++ cypress/integration/alura-pic/alura-pic.spec.js
@@ -5,5 +5,13 @@ describe("Login e registro de usuários alurapic", () => {
     cy.visit("http://alura-fotos.herokuapp.com/#/home");
   });
 
-  it("Verifica mensagem validação", () => {});
+  it("Verifica mensagem validação", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Email is required!").should("be.visible");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Full name is required!").should("be.visible");
+    cy.contains("ap-vmessage", "User name is required!").should("be.visible");
+    cy.contains("ap-vmessage", "Password is required!").should("be.visible");
+  });
 });
