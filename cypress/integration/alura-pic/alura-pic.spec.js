4ff528980074c734f01b0ba0dc07530b833774a2
Rubendvb <rubenvasquez556@gmail.com>
2022-04-24 03:34:25 -0300

    Adicionando projeto cypress da Alura

---
+++ cypress/integration/alura-pic/alura-pic.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("Login e registro de usuários alurapic", () => {
+  beforeEach(() => {
+    cy.visit("http://alura-fotos.herokuapp.com/#/home");
+  });
+
+  it("Verifica mensagem validação", () => {});
+});
