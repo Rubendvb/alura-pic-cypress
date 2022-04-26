746d181473bde9b14f25077576deed5d5ab521f9
Rubendvb <rubenvasquez556@gmail.com>
2022-04-26 02:02:46 -0300

    Criando massa de dados

--- cypress/integration/alura-pic/alura-pic.spec.js
+++ cypress/integration/alura-pic/alura-pic.spec.js
@@ -43,20 +43,21 @@ describe("Login e registro de usuários alurapic", () => {
   });
 
   // 5to Caso de teste
-  it.only("Fazer login de usuário válido", () => {
-    cy.get('input[formcontrolname="userName"]').type("flavio");
-    cy.get('input[formcontrolname="password"]').type("123");
-    cy.get("button[type='submit']").click();
+  it("Fazer login de usuário válido", () => {
+    cy.login("flavio", "123");
     cy.contains("a", "(Logout)").should("be.visible");
   });
 
   // 6to Caso de teste
-  it.only("Fazer login de usuário inválido", () => {
-    cy.get('input[formcontrolname="userName"]').type("jacqueline");
-    cy.get('input[formcontrolname="password"]').type("1234");
-    cy.get("button[type='submit']").click();
+  it("Fazer login de usuário inválido", () => {
+    cy.login("jaqueline", "1234");
     cy.on("window:alert", (str) => {
       expect(str).to.equal("Invalid user name or password");
     });
+  });
+
+  // 7to Caso de teste
+  it.only("Registro de novo usuário", () => {
+    cy.registro("teste@teste.com", "Ruben", "ruben12", "123456789");
   });
 });
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -3,3 +3,13 @@ Cypress.Commands.add("login", (nome, senha) => {
   cy.get('input[formcontrolname="password"]').type(senha);
   cy.get("button[type='submit']").click();
 });
+
+Cypress.Commands.add("registro", (email, nome, usuario, senha) => {
+  cy.contains("a", "Register now").click();
+  cy.contains("button", "Register").click();
+  cy.get('[formcontrolname="email"]').type(email);
+  cy.get('[formcontrolname="fullName"]').type(nome);
+  cy.get('[formcontrolname="userName"]').type(usuario);
+  cy.get('[formcontrolname="password"]').type(senha);
+  cy.contains("button", "Register").click();
+});
